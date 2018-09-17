69f034d4faf6af6f04d96a0c930c076934766cdd
Parvez <prvz786123@yahoo.com>
2018-09-17 13:40:23 +0530

    initial commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://prvz7861232:Testing123@ds245022.mlab.com:45022/mydatabse',{useNewUrlParser: true})
+mongoose.connect('mongodb://<username>:<password>@ds245022.mlab.com:45022/mydatabse',{useNewUrlParser: true})
 
 module.exports={
   mongoose
